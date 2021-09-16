ab99dab6b57aa50841658214ced7a5042b46bdbe
Raghav <70934347+raggzy271@users.noreply.github.com>
2021-09-16 20:15:23 +0530

    181A

---
+++ SeriesOfCrimes.java
@@ -0,0 +1,43 @@
+//	181 A - Series Of Crimes
+
+import java.util.Scanner;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+public class SeriesOfCrimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        HashMap<Integer, Integer> abscissas = new HashMap<>();
+        HashMap<Integer, Integer> ordinates = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String line = sc.next();
+            for (int j = 1; j <= m; j++) {
+                if (line.charAt(j - 1) == '*') {    
+                    abscissas.put(i, abscissas.getOrDefault(i, 0) + 1);
+                    ordinates.put(j, ordinates.getOrDefault(j, 0) + 1);
+                }
+            }
+        }
+
+        sc.close();
+
+        for (Entry entry : abscissas.entrySet()) {
+            if (entry.getValue().equals(1)) {
+                System.out.print(entry.getKey() + " ");
+                break;
+            }
+        }
+        
+        for (Entry entry : ordinates.entrySet()) {
+            if (entry.getValue().equals(1)) {
+                System.out.print(entry.getKey());
+                break;
+            }
+        }
+    }
+}
